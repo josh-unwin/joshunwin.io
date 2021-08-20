112032dbe98f1d831f3a9439b7f43baba380c244
Josh Unwin <joshunwin@protonmail.com>
2021-08-20 09:34:22 +0200

    tidied up homepage

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,14 @@ export default function Index({ posts, readingList }) {
       description="My little place on the internet 🤓"
     >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-10 text-black dark:text-white">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-6 text-black dark:text-white">
           Welcome 👋
         </h1>
-        {/* <p className="text-gray-600 dark:text-gray-400 mb-4">
-          Hi there, I write
-        </p> */}
+        <p className="text-gray-600 dark:text-gray-400 mb-10">
+          Welcome to my little corner of the internet where I occasionally write
+          stuff, experiment with some code, and share some things I find
+          interesting.
+        </p>
         <div className="mb-4">
           <h2 className="inline font-bold text-xl md:text-3xl tracking-tight text-black dark:text-white">
             Recent Posts
@@ -30,7 +32,7 @@ export default function Index({ posts, readingList }) {
           <BlogPost key={post.title} {...post} />
         ))}
 
-        <h2 className="font-bold text-xl md:text-3xl tracking-tight mb-4 text-black dark:text-white">
+        <h2 className="font-bold text-xl md:text-3xl tracking-tight mt-4 mb-4 text-black dark:text-white">
           Currently Reading
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-4">
